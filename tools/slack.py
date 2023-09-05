cecc547009bbe78cf456faa2d67be33649b8a292
Jordan Burgess <jordan@humanloop.com>
2023-09-05 16:41:10 +0100

    Update slack.py

--- tools/slack.py
+++ tools/slack.py
@@ -5,3 +5,6 @@ def no_action():
 
 def message_user(message: str):
     """Sends a message to the user offering to help them or confirming any detals."""
+    # This doesn't actually do anything here, because we still do this in main. It's
+    # just for signature and docstring here.
+    pass
