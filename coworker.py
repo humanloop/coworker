f6b2d12d95fc930b371b6eedc956ba2b552ff79b
Jordan Burgess <jordan@humanloop.com>
2023-09-08 22:19:28 +0100

    Use messages not prompt, names not IDs, and include attachments

--- coworker.py
+++ coworker.py
@@ -6,17 +6,24 @@ from typing import Callable, Optional
 
 from dotenv import load_dotenv
 from humanloop import Humanloop
+from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from slack_sdk import WebClient
 
 from tools.feedback import log_user_feedback
 from tools.linear import create_linear_issue, list_linear_teams
-from tools.slack import message_user, no_action, slack, web_client
+from tools.slack import _list_users, message_user, no_action
 from tools.utils import call_tool, parse_function
 
 load_dotenv()
+SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
+SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
+slack_client = WebClient(token=SLACK_BOT_TOKEN)
+app = App(token=SLACK_BOT_TOKEN)
 
 HUMANLOOP_API_KEY = os.getenv("HUMANLOOP_API_KEY")
 humanloop = Humanloop(api_key=HUMANLOOP_API_KEY)
+
 
 ENABLED_TOOLS = [
     no_action,
@@ -37,18 +44,21 @@ class Message:
 tool_schemas = [parse_function(t) for t in ENABLED_TOOLS]
 
 
-@slack.event("team_join")
+users = _list_users()
+
+
+@app.event("team_join")
 def team_join(body, say):
     print("team_join")
     pprint(body)
 
 
-@slack.event("app_home_opened")
+@app.event("app_home_opened")
 def handle_app_home_opened_events(body, logger):
     print(f"EVENT TYPE: {body['event']['type']}")
 
 
-@slack.event("message")
+@app.event("message")
 def handle_message(body: dict, say: Callable[[str], None]):
     """A message was sent to a channel"""
     print(
@@ -70,7 +80,7 @@ def handle_message(body: dict, say: Callable[[str], None]):
     return respond(body, say)
 
 
-@slack.event("app_mention")
+@app.event("app_mention")
 def handle_app_mentions(body: dict, say: Callable[[str], None]):
     print(f"EVENT TYPE: {body['event']['type']}")
 
@@ -101,7 +111,7 @@ def respond(body: dict, say: Callable[[str], None]):
     # Step 1: Fetch threaded messages if we're in a thread
     if thread_ts:
         print("thread_ts")
-        replies = web_client.conversations_replies(
+        replies = slack_client.conversations_replies(
             channel=channel,
             ts=thread_ts,
             limit=total_limit - 1,
@@ -116,7 +126,7 @@ def respond(body: dict, say: Callable[[str], None]):
 
     # Step 2: Fetch parent-level messages
     if total_limit - len(context_messages) > 0:
-        history = web_client.conversations_history(
+        history = slack_client.conversations_history(
             channel=channel,
             limit=total_limit - len(context_messages),
             latest=thread_ts if thread_ts else message_ts,
@@ -126,10 +136,27 @@ def respond(body: dict, say: Callable[[str], None]):
         context_messages += history["messages"]
 
     formatted_messages = []
+    # The latest messages is at the top
     for msg in context_messages:
         user = msg["user"]  # User is like 'U0124SFJGAD'
+        if msg.get("subtype") == "bot_message":
+            name = msg.get("username", "Bot")
+        else:
+            name = users.get(user, {}).get("name", "Unknown")
+
         timestamp = msg["ts"]  # Slack uses 'ts' for timestamps
         content = msg["text"]
+
+        # Check for attachments in the message
+        attachments_content = ""
+        if "attachments" in msg:
+            for attachment in msg["attachments"]:
+                # Here, we're including the title, pretext, and text from the attachment.
+                # Adjust as necessary.
+                attachment_title = attachment.get("title", "")
+                attachment_pretext = attachment.get("pretext", "")
+                attachment_text = attachment.get("text", "")
+                attachments_content += f"\n[Attachment: {attachment_title} {attachment_pretext} {attachment_text}]"
 
         # Convert timestamp to human-readable format
         # Slack's 'ts' is a string like '1623431161.000200' which you can split at the
@@ -138,15 +165,20 @@ def respond(body: dict, say: Callable[[str], None]):
             float(timestamp.split(".")[0])
         ).strftime("%Y-%m-%d %H:%M:%S UTC")
 
-        formatted_messages.append(f"{content} [{user} @ {readable_timestamp}]")
+        formatted_messages.append(
+            {
+                "role": "user" if msg.get("subtype") != "bot_message" else "system",
+                "name": name,
+                "content": f"{content}{attachments_content} [{readable_timestamp}]",
+            }
+        )
 
     print("\n\n")
-    pprint([msg[:40] for msg in formatted_messages])
+    pprint([f"{msg['name']}: {msg['content'][:40]}" for msg in formatted_messages])
     print("\n\n")
 
     # Join the messages to form the history string
     # Excluding the current message and go from oldest to newest
-    history = "\n".join(formatted_messages[1:][::-1])
     response = humanloop.chat(
         project="coworker/Brain",
         model_config={
@@ -167,25 +199,15 @@ The majority of messages should use the "no_action" function. Only use a differe
 function if you're very sure it will be useful as wse want to avoid bothering users. 
 
 ONLY CALL A FUNCTION, DO NOT RESPOND WITH TEXT. DEFAULT TO CALLING `no_action`.
-
-recent_chat_history:
-###
-{{history}}
-###
-
 """,
                 },
             ],
         },
-        inputs={"history": history},
-        messages=[
-            {"role": "user", "name": user, "content": formatted_messages[0]},
-        ],
+        messages=formatted_messages[0:][::-1],
     )
 
     chat_response = response.body["data"][0]
 
-    print(chat_response)
     if chat_response["finish_reason"] == "tool_call":
         tool_name = chat_response["tool_call"]["name"]
         args = json.loads(chat_response["tool_call"]["arguments"])
@@ -197,23 +219,14 @@ recent_chat_history:
 
     print("→ ", new_message_text)
     if new_message_text:
-        # web_client.chat_update(
-        #     channel=channel,
-        #     ts=response_message["ts"],
-        #     text=new_message_text,
-        #     thread_ts=thread_ts,
-        # )
-        web_client.chat_postMessage(
+        slack_client.chat_postMessage(
             channel=channel,
             text=new_message_text,
             thread_ts=thread_ts if thread_ts else message_ts,
         )
-
-    # else:
-    # web_client.chat_delete(channel=channel, ts=response_message["ts"])
     return "OK"
 
 
 if __name__ == "__main__":
-    handler = SocketModeHandler(slack, os.getenv("SLACK_APP_TOKEN"))
+    handler = SocketModeHandler(app, os.getenv("SLACK_APP_TOKEN"))
     handler.start()
--- tools/slack.py
+++ tools/slack.py
@@ -2,6 +2,7 @@
 import json
 import os
 from pprint import pprint
+from typing import Dict
 
 from dotenv import load_dotenv
 from slack_bolt import App
@@ -30,18 +31,46 @@ def list_channels() -> str:
     cursor = None
     channels = {}
     while True:
-        response = web_client.conversations_list(cursor=cursor)
+        response = web_client.conversations_list(
+            cursor=cursor, limit=100, exclude_archived=True, types="public_channel"
+        )
         if response["ok"]:
             for channel in response["channels"]:
                 channels[channel["name"]] = channel
+            cursor = response["response_metadata"]["next_cursor"]
+            print(cursor)
+            if not cursor:
+                break
+        else:
+            print(f'Error: {response["error"]}')
+            break
+    return json.dumps([f"{k} {v['id']}" for k, v in channels.items()])
+
+
+def _list_users() -> Dict[str, Dict]:
+    cursor = None
+    users = {}
+    while True:
+        response = web_client.users_list(cursor=cursor, limit=200)
+        if response["ok"]:
+            for user in response.get("members", []):
+                # Filter out bots and deleted users
+                if not user.get("is_bot") and not user.get("deleted"):
+                    users[user["id"]] = user
             cursor = response["response_metadata"]["next_cursor"]
             if not cursor:
                 break
         else:
             print(f'Error: {response["error"]}')
             break
-    return json.dumps([f"{k} {v['id']}" for k, v in list_channels().items()])
+    return users
+
+
+def list_users() -> str:
+    """List the users in the workspace"""
+    return json.dumps([f"{k} {v['name']}" for k, v in _list_users().items()])
 
 
 if __name__ == "__main__":
-    pprint(list_channels())
+    # pprint(list_channels())
+    pprint(list_users())
